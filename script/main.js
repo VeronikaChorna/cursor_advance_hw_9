fd2d2918e61b0048c8a05fa5d2e2a4ab8aff45c7
nika_chorna <veronika.chorna2000@gmail.com>
2023-01-23 16:22:40 +0200

    modify code

--- script/main.js
+++ script/main.js
@@ -1,28 +1,33 @@
-const button = document.querySelector('.btn');
-
-button.onclick = generateBlocks();
-
-
-
 function generateBlocks() { 
     const divsAmound = 25;
     const container = document.querySelector(".container");
-   
-    const setDivBg = () => {
-        const randomColor = Math.floor(Math.random()*16777215).toString(16);
-        return randomColor;
-      }
-
     for (let i = 0; i < divsAmound; i++) {
        div = document.createElement('div');
        div.id = 'block' + i;
        div.style.backgroundColor = "#" + setDivBg();
        div.className = 'box';
        container.prepend(div);
-        // divs = document.getElementsByClassName('box');
-    }
-    
+    }   
 }
+
+const setDivBg = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
+  }
+
+function generateBlocksInterval() {
+    generateBlocks();
+    setInterval(() => {
+        for (let i = 0; i < 25; i++) {
+            let b = document.getElementById('block' + i);
+            b.style.backgroundColor = "#" + setDivBg();
+        }
+    }, 1000);
+}
+
+document.getElementById("clickMe").onclick = generateBlocksInterval;
+
+
 
 
 
